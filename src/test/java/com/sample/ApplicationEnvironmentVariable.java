ecf3597db21b83e1fb53c454de01208626b41dc6
ashutosh <ashutosh286@gmail.com>
2020-11-04 02:41:28 +0530

    ApplicationEnvironmentVariable.java added

--- src/test/java/com/sample/ApplicationEnvironmentVariable.java
+++ src/test/java/com/sample/ApplicationEnvironmentVariable.java
@@ -5,8 +5,8 @@ import net.thucydides.core.util.EnvironmentVariables;
 
 public class ApplicationEnvironmentVariable {
     private static EnvironmentVariables environmentVariables;
-    public static final String API_STATUS_BASE_URL =  EnvironmentSpecificConfiguration.from(environmentVariables)
+    public static final String API_STATUS_BASE_URL = EnvironmentSpecificConfiguration.from(environmentVariables)
             .getProperty("environments.qa.api.status.base.url");
-    public static final String API_TRADE_BASE_URL =  EnvironmentSpecificConfiguration.from(environmentVariables)
+    public static final String API_TRADE_BASE_URL = EnvironmentSpecificConfiguration.from(environmentVariables)
             .getProperty("environments.qa.api.trade.base.url");
 }
